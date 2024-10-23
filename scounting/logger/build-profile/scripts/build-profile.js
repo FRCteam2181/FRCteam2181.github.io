a072364c77901f85a3c83b227a0926d0049c71df
Voltron42 <dajohnson1978@gmail.com>
2024-10-23 09:39:54 -0500

    initial stub of scouting logger app suite

---
+++ scounting/logger/build-profile/scripts/build-profile.js
@@ -0,0 +1,11 @@
+namespace("2181robotics.scouting.logger.build-profile.BuildProfile", () => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    render() {
+      return (<></>);
+    }
+  }
+});
---
+++ scounting/logger/record-profile-convert/scripts/record-profile-convert.js
@@ -0,0 +1,11 @@
+namespace("2181robotics.scouting.logger.record-profile-convert.RecordProfileConvert", () => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    render() {
+      return (<></>);
+    }
+  }
+});
